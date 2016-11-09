f5284331f65fbf525f717d9b4ddd9c6075c81865
Joseph Blank <joe.blank@live.com>
2016-11-09 15:53:04 -0700

    pencil parallax and side text completed

--- public/app/pencil/pencilRotation.js
+++ public/app/pencil/pencilRotation.js
@@ -17,7 +17,7 @@ if (0 < winScroll) {
   $('.p-2, .p-3, .p-4, .p-5, .p-6, .p-7, .p-8, .p-9, .p-10, .p-11').css({
     "z-index": "-1"
   })
-};
+}
 if (scrollLocation < winScroll) {
   $('.p-2').css({
     "z-index": "2"
@@ -25,7 +25,7 @@ if (scrollLocation < winScroll) {
   $('.p-1, .p-3, .p-4, .p-5, .p-6, .p-7, .p-8, .p-9, .p-10, .p-11').css({
     "z-index": "-1"
   })
-};
+}
 if ((scrollLocation + (scrollIncrement * 2)) < winScroll) {
   $('.p-3').css({
     "z-index": "2"
@@ -33,7 +33,7 @@ if ((scrollLocation + (scrollIncrement * 2)) < winScroll) {
   $('.p-1, .p-2, .p-4, .p-5, .p-6, .p-7, .p-8, .p-9, .p-10, .p-11').css({
     "z-index": "-1"
   })
-};
+}
 if ((scrollLocation + (scrollIncrement * 3)) < winScroll) {
   $('.p-4').css({
     "z-index": "2"
@@ -41,7 +41,7 @@ if ((scrollLocation + (scrollIncrement * 3)) < winScroll) {
   $('.p-1, .p-2, .p-3, .p-5, .p-6, .p-7, .p-8, .p-9, .p-10, .p-11').css({
     "z-index": "-1"
   })
-};
+}
 if ((scrollLocation + (scrollIncrement * 4)) < winScroll) {
   $('.p-5').css({
     "z-index": "2"
@@ -49,7 +49,7 @@ if ((scrollLocation + (scrollIncrement * 4)) < winScroll) {
   $('.p-1, .p-2, .p-3, .p-4, .p-6, .p-7, .p-8, .p-9, .p-10, .p-11').css({
     "z-index": "-1"
   })
-};
+}
 if ((scrollLocation + (scrollIncrement * 5)) < winScroll) {
   $('.p-6').css({
     "z-index": "2"
@@ -57,7 +57,7 @@ if ((scrollLocation + (scrollIncrement * 5)) < winScroll) {
   $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-7, .p-8, .p-9, .p-10, .p-11').css({
     "z-index": "-1"
   })
-};
+}
 if ((scrollLocation + (scrollIncrement * 6)) < winScroll) {
   $('.p-7').css({
     "z-index": "2"
@@ -65,7 +65,7 @@ if ((scrollLocation + (scrollIncrement * 6)) < winScroll) {
   $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-6, .p-8, .p-9, .p-10, .p-11').css({
     "z-index": "-1"
   })
-};
+}
 if ((scrollLocation + (scrollIncrement * 7)) < winScroll) {
   $('.p-8').css({
     "z-index": "2"
@@ -73,7 +73,7 @@ if ((scrollLocation + (scrollIncrement * 7)) < winScroll) {
   $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-6, .p-7, .p-9, .p-10, .p-11').css({
     "z-index": "-1"
   })
-};
+}
 if ((scrollLocation + (scrollIncrement * 8)) < winScroll) {
   $('.p-9').css({
     "z-index": "2"
@@ -81,7 +81,7 @@ if ((scrollLocation + (scrollIncrement * 8)) < winScroll) {
   $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-6, .p-7, .p-8, .p-10, .p-11').css({
     "z-index": "-1"
   })
-};
+}
 if ((scrollLocation + (scrollIncrement * 9)) < winScroll) {
   $('.p-10').css({
     "z-index": "2"
@@ -89,7 +89,7 @@ if ((scrollLocation + (scrollIncrement * 9)) < winScroll) {
   $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-6, .p-7, .p-8, .p-9, .p-11').css({
     "z-index": "-1"
   })
-};
+}
 if ((scrollLocation + (scrollIncrement * 10)) < winScroll) {
   $('.p-11').css({
     "z-index": "2"
@@ -97,112 +97,130 @@ if ((scrollLocation + (scrollIncrement * 10)) < winScroll) {
   $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-6, .p-7, .p-8, .p-9, .p-10').css({
     "z-index": "-1"
   })
-};
+}
 
 
 // PARALLAX
 
 //BLUETOOTH
-if(winScroll > 4899) {
+
+const parallaxRatio = 4899;
+//parallaxRatio variable not fully implemented yet
+// ^^ DO NOT USE ^^
+
+if(winScroll > parallaxRatio) {
   $('.bluetooth').css({
     'position': 'fixed',
-    'top': '512px'
+    'top': (parallaxRatio - 4387)
   })
 } else {
   $('.bluetooth').css({
     'position': 'static'
   })
 };
-if (winScroll > 5049) {
+if (winScroll > parallaxRatio + 150) {
   $('.bluetooth').css({
     'position': 'absolute',
-    'top': '263px'
+    'top': (parallaxRatio - 4636)
   })
 };
 // BODY
-if (winScroll > 4889) {
+if (winScroll > parallaxRatio - 10) {
   $('.body').css({
     'position': 'fixed',
-    'top': '522px'
+    'top': (parallaxRatio - 4377)
   })
 } else {
   $('.body').css({
     'position': 'absolute',
-    'top': '115px'
+    'top': (parallaxRatio - 4784)
   })
 }
-if (winScroll > 5400) {
+if (winScroll > parallaxRatio + 501) {
   $('.body').css({
     'position': 'absolute',
-    'top': '628px'
+    'top': (parallaxRatio - 4271)
   })
 }
 // BATTERY
-if (winScroll > 4899) {
+if (winScroll > parallaxRatio) {
   $('.battery').css({
     'position': 'fixed',
-    'top': '512px'
+    'top': (parallaxRatio - 4387)
   })
 } else {
   $('.battery').css({
     'position': 'static'
   })
 };
-if (winScroll > 5780) {
+if (winScroll > parallaxRatio + 881) {
   $('.battery').css({
     'position': 'absolute',
-    'top': '1097px'
+    'top': (parallaxRatio + 3802)
   })
 };
-if (winScroll > 5717) {
+if (winScroll > parallaxRatio + 818) {
   $('.battery').css({
     'position': 'fixed',
-    'top': '202px'
+    'top': (parallaxRatio - 4697)
   })
 };
-if (winScroll > 6355) {
+if (winScroll > parallaxRatio + 1456) {
   $('.battery').css({
     'position': 'absolute',
-    'top': '1260px'
+    'top': (parallaxRatio - 3639)
   })
 }
 
 // SENSOR
-if(winScroll > 4899) {
+if(winScroll > parallaxRatio) {
   $('.sensor').css({
     'position': 'fixed',
-    'top': '512px'
+    'top': (parallaxRatio - 4387)
   })
 } else {
   $('.sensor').css({
     'position': 'static'
   })
 };
-if (winScroll > 5780) {
+if (winScroll > parallaxRatio + 881) {
   $('.sensor').css({
     'position': 'absolute',
-    'top': '1097px'
+    'top': (parallaxRatio - 3809)
   })
 };
-if (winScroll > 5924) {
+if (winScroll > parallaxRatio + 1025) {
   $('.sensor').css({
     'position': 'fixed',
-    'top': '461px'
+    'top': (parallaxRatio - 4438)
   })
 };
-if (winScroll > 6650) {
+if (winScroll > 6351) {
   $('.sensor').css({
     'position': 'absolute',
-    'top': '1650px'
+    'top': '1515px',
+    // 'transform': 'translate( 0px, ' + (winScroll / 1000) + '%)'
+  })
+}
+if (winScroll > 6570) {
+  $('.sensor').css({
+    'position': 'fixed',
+    'top': '241px'
+  })
+}
+if (winScroll > 6725) {
+  $('.sensor').css({
+    'position': 'absolute',
+    'top': '1670px'
   })
 }
 
 
 // TIP
-if(winScroll > 4899) {
+if(winScroll > parallaxRatio) {
   $('.tip').css({
     'position': 'fixed',
-    'top': '512px'
+    'top': (parallaxRatio - 4387)
   })
 } else {
   $('.tip').css({
@@ -221,15 +239,85 @@ if (winScroll > 5924) {
     'top': '468px'
   })
 };
-if (winScroll > 6650) {
+if (winScroll > 6700) {
   $('.tip').css({
     'position': 'absolute',
-    'top': '1825px'
+    'top': '1875px'
   })
+}
+
+//HIDDEN TEXT FOR PARALLAX
+
+//eraser
+if (winScroll > 4933) {
+  $('.left-eraser').fadeIn(600)
+} else if (winScroll <= 4933) {
+  $('.left-eraser').fadeOut(600)
+}
+if (winScroll > 4935) {
+  $('.right-eraser').fadeIn(600)
+} else if (winScroll <= 4935) {
+  $('.right-eraser').fadeOut(600)
+}
+//bluetooth
+if (winScroll > 5125) {
+  $('.left-bluetooth').fadeIn(600)
+} else if (winScroll <= 5125) {
+  $('.left-bluetooth').fadeOut(600)
+}
+if (winScroll > 5127) {
+  $('.right-bluetooth').fadeIn(600)
+} else if (winScroll <= 5127) {
+  $('.right-bluetooth').fadeOut(600)
+}
+//body
+if (winScroll > 5470) {
+  $('.left-body').fadeIn(600)
+} else if (winScroll <= 5470) {
+  $('.left-body').fadeOut(600)
+}
+if (winScroll > 5472) {
+  $('.right-body').fadeIn(600)
+} else if (winScroll <= 5472) {
+  $('.right-body').fadeOut(600)
+}
+//battery
+if (winScroll > 6145) {
+  $('.left-battery').fadeIn(600)
+} else if (winScroll <= 6145) {
+  $('.left-battery').fadeOut(600)
+}
+if (winScroll > 6147) {
+  $('.right-battery').fadeIn(600)
+} else if (winScroll <= 6147) {
+  $('.right-battery').fadeOut(600)
+}
+//sensor
+if (winScroll > 6548) {
+  $('.left-sensor').fadeIn(600)
+} else if (winScroll <= 6548) {
+  $('.left-sensor').fadeOut(600)
+}
+if (winScroll > 6550) {
+  $('.right-sensor').fadeIn(600)
+} else if (winScroll <= 6550) {
+  $('.right-sensor').fadeOut(600)
+}
+//tip
+if (winScroll > 6738) {
+  $('.left-tip').fadeIn(600)
+} else if (winScroll <= 6738) {
+  $('.left-tip').fadeOut(600)
+}
+if (winScroll > 6740) {
+  $('.right-tip').fadeIn(600)
+} else if (winScroll <= 6740) {
+  $('.right-tip').fadeOut(600)
 }
 
 
 
 
 
+// END
 })
--- public/dist/all.js
+++ public/dist/all.js
@@ -228,7 +228,7 @@ $(window).scroll(function () {
     $('.p-2, .p-3, .p-4, .p-5, .p-6, .p-7, .p-8, .p-9, .p-10, .p-11').css({
       "z-index": "-1"
     });
-  };
+  }
   if (scrollLocation < winScroll) {
     $('.p-2').css({
       "z-index": "2"
@@ -236,7 +236,7 @@ $(window).scroll(function () {
     $('.p-1, .p-3, .p-4, .p-5, .p-6, .p-7, .p-8, .p-9, .p-10, .p-11').css({
       "z-index": "-1"
     });
-  };
+  }
   if (scrollLocation + scrollIncrement * 2 < winScroll) {
     $('.p-3').css({
       "z-index": "2"
@@ -244,7 +244,7 @@ $(window).scroll(function () {
     $('.p-1, .p-2, .p-4, .p-5, .p-6, .p-7, .p-8, .p-9, .p-10, .p-11').css({
       "z-index": "-1"
     });
-  };
+  }
   if (scrollLocation + scrollIncrement * 3 < winScroll) {
     $('.p-4').css({
       "z-index": "2"
@@ -252,7 +252,7 @@ $(window).scroll(function () {
     $('.p-1, .p-2, .p-3, .p-5, .p-6, .p-7, .p-8, .p-9, .p-10, .p-11').css({
       "z-index": "-1"
     });
-  };
+  }
   if (scrollLocation + scrollIncrement * 4 < winScroll) {
     $('.p-5').css({
       "z-index": "2"
@@ -260,7 +260,7 @@ $(window).scroll(function () {
     $('.p-1, .p-2, .p-3, .p-4, .p-6, .p-7, .p-8, .p-9, .p-10, .p-11').css({
       "z-index": "-1"
     });
-  };
+  }
   if (scrollLocation + scrollIncrement * 5 < winScroll) {
     $('.p-6').css({
       "z-index": "2"
@@ -268,7 +268,7 @@ $(window).scroll(function () {
     $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-7, .p-8, .p-9, .p-10, .p-11').css({
       "z-index": "-1"
     });
-  };
+  }
   if (scrollLocation + scrollIncrement * 6 < winScroll) {
     $('.p-7').css({
       "z-index": "2"
@@ -276,7 +276,7 @@ $(window).scroll(function () {
     $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-6, .p-8, .p-9, .p-10, .p-11').css({
       "z-index": "-1"
     });
-  };
+  }
   if (scrollLocation + scrollIncrement * 7 < winScroll) {
     $('.p-8').css({
       "z-index": "2"
@@ -284,7 +284,7 @@ $(window).scroll(function () {
     $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-6, .p-7, .p-9, .p-10, .p-11').css({
       "z-index": "-1"
     });
-  };
+  }
   if (scrollLocation + scrollIncrement * 8 < winScroll) {
     $('.p-9').css({
       "z-index": "2"
@@ -292,7 +292,7 @@ $(window).scroll(function () {
     $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-6, .p-7, .p-8, .p-10, .p-11').css({
       "z-index": "-1"
     });
-  };
+  }
   if (scrollLocation + scrollIncrement * 9 < winScroll) {
     $('.p-10').css({
       "z-index": "2"
@@ -300,7 +300,7 @@ $(window).scroll(function () {
     $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-6, .p-7, .p-8, .p-9, .p-11').css({
       "z-index": "-1"
     });
-  };
+  }
   if (scrollLocation + scrollIncrement * 10 < winScroll) {
     $('.p-11').css({
       "z-index": "2"
@@ -308,110 +308,127 @@ $(window).scroll(function () {
     $('.p-1, .p-2, .p-3, .p-4, .p-5, .p-6, .p-7, .p-8, .p-9, .p-10').css({
       "z-index": "-1"
     });
-  };
+  }
 
   // PARALLAX
 
   //BLUETOOTH
-  if (winScroll > 4899) {
+
+  var parallaxRatio = 4899;
+  //parallaxRatio variable not fully implemented yet
+  // ^^ DO NOT USE ^^
+
+  if (winScroll > parallaxRatio) {
     $('.bluetooth').css({
       'position': 'fixed',
-      'top': '512px'
+      'top': parallaxRatio - 4387
     });
   } else {
     $('.bluetooth').css({
       'position': 'static'
     });
   };
-  if (winScroll > 5049) {
+  if (winScroll > parallaxRatio + 150) {
     $('.bluetooth').css({
       'position': 'absolute',
-      'top': '263px'
+      'top': parallaxRatio - 4636
     });
   };
   // BODY
-  if (winScroll > 4889) {
+  if (winScroll > parallaxRatio - 10) {
     $('.body').css({
       'position': 'fixed',
-      'top': '522px'
+      'top': parallaxRatio - 4377
     });
   } else {
     $('.body').css({
       'position': 'absolute',
-      'top': '115px'
+      'top': parallaxRatio - 4784
     });
   }
-  if (winScroll > 5400) {
+  if (winScroll > parallaxRatio + 501) {
     $('.body').css({
       'position': 'absolute',
-      'top': '628px'
+      'top': parallaxRatio - 4271
     });
   }
   // BATTERY
-  if (winScroll > 4899) {
+  if (winScroll > parallaxRatio) {
     $('.battery').css({
       'position': 'fixed',
-      'top': '512px'
+      'top': parallaxRatio - 4387
     });
   } else {
     $('.battery').css({
       'position': 'static'
     });
   };
-  if (winScroll > 5780) {
+  if (winScroll > parallaxRatio + 881) {
     $('.battery').css({
       'position': 'absolute',
-      'top': '1097px'
+      'top': parallaxRatio + 3802
     });
   };
-  if (winScroll > 5717) {
+  if (winScroll > parallaxRatio + 818) {
     $('.battery').css({
       'position': 'fixed',
-      'top': '202px'
+      'top': parallaxRatio - 4697
     });
   };
-  if (winScroll > 6355) {
+  if (winScroll > parallaxRatio + 1456) {
     $('.battery').css({
       'position': 'absolute',
-      'top': '1260px'
+      'top': parallaxRatio - 3639
     });
   }
 
   // SENSOR
-  if (winScroll > 4899) {
+  if (winScroll > parallaxRatio) {
     $('.sensor').css({
       'position': 'fixed',
-      'top': '512px'
+      'top': parallaxRatio - 4387
     });
   } else {
     $('.sensor').css({
       'position': 'static'
     });
   };
-  if (winScroll > 5780) {
+  if (winScroll > parallaxRatio + 881) {
     $('.sensor').css({
       'position': 'absolute',
-      'top': '1097px'
+      'top': parallaxRatio - 3809
     });
   };
-  if (winScroll > 5924) {
+  if (winScroll > parallaxRatio + 1025) {
     $('.sensor').css({
       'position': 'fixed',
-      'top': '461px'
+      'top': parallaxRatio - 4438
     });
   };
-  if (winScroll > 6650) {
+  if (winScroll > 6351) {
     $('.sensor').css({
       'position': 'absolute',
-      'top': '1650px'
+      'top': '1515px'
+    });
+  }
+  if (winScroll > 6570) {
+    $('.sensor').css({
+      'position': 'fixed',
+      'top': '241px'
+    });
+  }
+  if (winScroll > 6725) {
+    $('.sensor').css({
+      'position': 'absolute',
+      'top': '1670px'
     });
   }
 
   // TIP
-  if (winScroll > 4899) {
+  if (winScroll > parallaxRatio) {
     $('.tip').css({
       'position': 'fixed',
-      'top': '512px'
+      'top': parallaxRatio - 4387
     });
   } else {
     $('.tip').css({
@@ -430,12 +447,83 @@ $(window).scroll(function () {
       'top': '468px'
     });
   };
-  if (winScroll > 6650) {
+  if (winScroll > 6700) {
     $('.tip').css({
       'position': 'absolute',
-      'top': '1825px'
+      'top': '1875px'
     });
   }
+
+  //HIDDEN TEXT FOR PARALLAX
+
+  //eraser
+  if (winScroll > 4933) {
+    $('.left-eraser').fadeIn(600);
+  } else if (winScroll <= 4933) {
+    $('.left-eraser').fadeOut(600);
+  }
+  if (winScroll > 4935) {
+    $('.right-eraser').fadeIn(600);
+  } else if (winScroll <= 4935) {
+    $('.right-eraser').fadeOut(600);
+  }
+  //bluetooth
+  if (winScroll > 5125) {
+    $('.left-bluetooth').fadeIn(600);
+  } else if (winScroll <= 5125) {
+    $('.left-bluetooth').fadeOut(600);
+  }
+  if (winScroll > 5127) {
+    $('.right-bluetooth').fadeIn(600);
+  } else if (winScroll <= 5127) {
+    $('.right-bluetooth').fadeOut(600);
+  }
+  //body
+  if (winScroll > 5470) {
+    $('.left-body').fadeIn(600);
+  } else if (winScroll <= 5470) {
+    $('.left-body').fadeOut(600);
+  }
+  if (winScroll > 5472) {
+    $('.right-body').fadeIn(600);
+  } else if (winScroll <= 5472) {
+    $('.right-body').fadeOut(600);
+  }
+  //battery
+  if (winScroll > 6145) {
+    $('.left-battery').fadeIn(600);
+  } else if (winScroll <= 6145) {
+    $('.left-battery').fadeOut(600);
+  }
+  if (winScroll > 6147) {
+    $('.right-battery').fadeIn(600);
+  } else if (winScroll <= 6147) {
+    $('.right-battery').fadeOut(600);
+  }
+  //sensor
+  if (winScroll > 6548) {
+    $('.left-sensor').fadeIn(600);
+  } else if (winScroll <= 6548) {
+    $('.left-sensor').fadeOut(600);
+  }
+  if (winScroll > 6550) {
+    $('.right-sensor').fadeIn(600);
+  } else if (winScroll <= 6550) {
+    $('.right-sensor').fadeOut(600);
+  }
+  //tip
+  if (winScroll > 6738) {
+    $('.left-tip').fadeIn(600);
+  } else if (winScroll <= 6738) {
+    $('.left-tip').fadeOut(600);
+  }
+  if (winScroll > 6740) {
+    $('.right-tip').fadeIn(600);
+  } else if (winScroll <= 6740) {
+    $('.right-tip').fadeOut(600);
+  }
+
+  // END
 });
 'use strict';
 
